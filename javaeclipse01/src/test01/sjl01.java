beb9efd1d009edcaca688241ce99c2db47172f47
vostory <xiaoyun20601@gmail.com>
2019-01-03 21:31:07 +0800

    Create new case
    Create new case

---
+++ javaeclipse01/src/test01/sjl01.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test01;
+
+/**
+ * @author Administrator
+ *
+ */
+public class sjl01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
